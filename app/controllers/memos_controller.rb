1e02d5d16b065cb5cfeb344ec36f6b01eb726848
磯部浩樹 <h.sep23@gmail.com>
2020-12-11 17:39:23 +0900

    [Add]投稿機能

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,23 @@
+class MemosController < ApplicationController
+
+  def index
+    @memos = Memo.all
+  end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.create(memo_params)
+    redirect_to memos_path
+  end
+
+
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:title, :content)
+  end
+end
